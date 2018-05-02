51bfdf7ea94c3cc23f6805c1aaa3234f3604beb5
Rancool0 <38376196+Rancool0@users.noreply.github.com>
2018-05-02 11:50:03 +0800

    Fix the issue about can't write data

--- url_data.py
+++ url_data.py
@@ -5,11 +5,9 @@ class UrlData():
 
     def wiret_data(self,file_obj,data,page_url):
         #写文件
-            if page_url or data is None:
-                return
-            file_obj.write(page_url+'\n')
-            file_obj.write('name:'+data['name'])
-            file_obj.write('info:'+data['info'])
-            file_obj.write('detail:'+data['detail']+'\n')
-            file_obj.write('score:'+data['score']+'\n'+'\n')
+        file_obj.write(page_url+'\n')
+        file_obj.write('name:'+data['name'])
+        file_obj.write('info:'+data['info'])
+        file_obj.write('detail:'+data['detail']+'\n')
+        file_obj.write('score:'+data['score']+'\n'+'\n')
 
